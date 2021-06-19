d84b864cbaca44b1259fcd7045b7e53703dcfe7a
minimiM <minimiGamesAM@gmail.com>
2021-06-18 21:08:56 -0400

    Move pawn.

--- Source/unrealPrototypeGame/MainCharacterMovementComponent.cpp
+++ Source/unrealPrototypeGame/MainCharacterMovementComponent.cpp
@@ -1,18 +1,51 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MainCharacterMovementComponent.h"
+
+UMainCharacterMovementComponent::UMainCharacterMovementComponent()
+{
+	MaxAcceleration = 1000.0f;
+	Acceleration = FVector::ZeroVector;
+}
 
 void UMainCharacterMovementComponent::MoveForward(float AxisValue)
 {
-	if( AxisValue != 0.0)
-	UE_LOG(LogTemp, Warning, TEXT("Move forward activated %f"), AxisValue);
-	
-	//return 0.0f;
+	if (PawnOwner && UpdatedComponent == PawnOwner->GetRootComponent())
+	{
+		AddInputVector(PawnOwner->GetActorForwardVector() * AxisValue);
+	}
 }
 
 void UMainCharacterMovementComponent::MoveRight(float AxisValue)
 {
 	if (AxisValue != 0.0)
 	UE_LOG(LogTemp, Warning, TEXT("Move right activated %f"), AxisValue);
+}
+
+void UMainCharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!PawnOwner || !UpdatedComponent || ShouldSkipUpdate(DeltaTime))
+	{
+		return;
+	}
+
+	const FVector InputVector = ConsumeInputVector().GetClampedToMaxSize(1.0f);
+	Acceleration += InputVector * MaxAcceleration;
+
+	Velocity += Acceleration * DeltaTime;
+
+	FVector DesiredMovementThisFrame = Velocity * DeltaTime;
+
+	if (!DesiredMovementThisFrame.IsNearlyZero())
+	{
+		FRotator CurrentRotation;
+		MoveUpdatedComponent(DesiredMovementThisFrame, CurrentRotation, true);
+	}
+		
+	//Damping
+	Acceleration *= 0.65f;
+	Velocity *= 0.95f;
+
 }
--- Source/unrealPrototypeGame/MainCharacterMovementComponent.h
+++ Source/unrealPrototypeGame/MainCharacterMovementComponent.h
@@ -16,7 +16,17 @@ class UNREALPROTOTYPEGAME_API UMainCharacterMovementComponent : public UPawnMove
 	
 public:
 
+	UMainCharacterMovementComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	//UFUNCTION(BlueprintCallable, Category = "Input")
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+
+	UPROPERTY(EditAnywhere, Category = "Movement", BlueprintReadOnly)
+	float MaxAcceleration;
+
+protected:
+	FVector Acceleration;
 };
