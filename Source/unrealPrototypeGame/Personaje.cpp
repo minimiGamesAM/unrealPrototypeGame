b64cbd4143f62b80a12dc1777a4182f2badc229d
minimiM <minimiGamesAM@gmail.com>
2021-06-22 23:12:35 -0400

    Based personaje de caracter class.

---
+++ Source/unrealPrototypeGame/Personaje.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Personaje.h"
+
+// Sets default values
+APersonaje::APersonaje()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APersonaje::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APersonaje::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APersonaje::MoveForward(float AxisValue)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, AxisValue);
+}
+
+void APersonaje::MoveRight(float AxisValue)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, AxisValue);
+}
+
+// Called to bind functionality to input
+void APersonaje::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	PlayerInputComponent->BindAxis("MoveForward", this, &APersonaje::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APersonaje::MoveRight);
+}
+
---
+++ Source/unrealPrototypeGame/Personaje.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Personaje.generated.h"
+
+UCLASS()
+class UNREALPROTOTYPEGAME_API APersonaje : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APersonaje();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	
+	//Input
+	UFUNCTION()
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+};
